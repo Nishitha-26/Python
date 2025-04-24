bfa0be45ebeffd518dcea0e07308a07e6c874ddd
Nishitha-26 <ynishithareddy@gmail.com>
2025-04-24 09:33:55 +0530

    Characters count script

--- number_of_characters_in_a_string.py
+++ number_of_characters_in_a_string.py
@@ -1,15 +1,65 @@
-def number_of_chars(string):
+#  Excludes spaces using a loop
+def number_of_chars(s):
     """
-    Counting number of characters in a string excluding space
-    :param string: str: Takes input as a string
-    :return: int: Returns the count of characters
+    Counts number of characters in a string excluding spaces.
+    :param s: str: Takes input as a string.
+    :return: int: Returns the count of characters.
     """
     count = 0
-    for i in string:
+    for i in s:
         if i != " ":
             count += 1
     return count
 
-string = "Python is a popular programming language"
-print(number_of_chars(string))
 
+s = "Python is a popular programming language"
+print(number_of_chars(s))
+
+
+# Excludes spaces using replace() and len()
+
+def number_of_characters(s):
+    """
+     Counts number of characters in a string excluding spaces.
+    :param s: str: Takes input as a string.
+    :return: int: Returns the count of characters.
+    """
+    a = s.replace(" ", "")
+    return len(a)
+
+
+s = "Python is a popular programming language"
+print(number_of_characters(s))
+
+
+#  Includes spaces using a loop
+
+def number_of_characters(s):
+    """
+    Counts number of characters in a given string including spaces.
+    :param s: str: Takes input as a string.
+    :return: int : Returns the count of  each character.
+    """
+    count = 0
+    for i in s:
+        count += 1
+    return count
+
+
+s = "Python is a popular programming language"
+print(number_of_characters(s))
+
+
+#  Includes spaces using len()
+
+def number_of_characters(s):
+    """
+    Counts number of characters in a given string including spaces.
+    :param s: str: Takes input as a string.
+    :return: int : Returns the count of  each character.
+    """
+    return len(s)
+
+
+s = "Python is a popular programming language"
+print(number_of_characters(s))
